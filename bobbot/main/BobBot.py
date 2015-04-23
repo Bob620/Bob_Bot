f714a8de54c9faa4fb7fc96c555bc799d90cc361
Bob620 <bruder.kraft225@gmail.com>
2015-04-23 17:43:29 -0500

    Changing Module Loading -1-

--- bobbot/main/BobBot.py
+++ bobbot/main/BobBot.py
@@ -16,12 +16,43 @@ import collections
 import irc
 import urllib.request
 import imp
+import pprint
 
-fileInteraction = imp.new_module('fileInteraction')
-exec(urllib.request.urlopen('https://raw.githubusercontent.com/Bob620/Bob_Bot/master/bobbot/modules/fileInteraction.py').read().decode('utf8'), fileInteraction.__dict__)
-
-
-__module_name__ = "WaifuBot for Toka"
-__module_version__ = "1.0"
-__module_description__ = "Version 1.0"
+__module_name__ = "BobBot V20"
+__module_version__ = "20"
+__module_description__ = "The 20th version of BobBot made by Bob620, I may have skipped a few numbers but it really is close to that >.>... At least every version gets better, usually."
 __module_author__ = "Bob620"
+
+global modules
+modules = {"required":[], "all":[], "extra":[]}
+global Running_Modules
+Running_Modules = imp.new_module('Running_Modules')
+
+def Importation(module):
+
+	if modules['all'].count('fileInteraction') < 1:
+		exec(urllib.request.urlopen('https://raw.githubusercontent.com/Bob620/Bob_Bot/master/bobbot/modules/fileInteraction.py').read().decode('utf8'), Running_Modules.__dict__)
+		modules['required'].append('fileInteraction')
+		modules['all'].append('fileInteraction')
+
+	if module == 'required':
+		Required_Modules = Running_Modules.fileInteraction.readfile('info', 'json')['modules']['required']
+		for ModuleName in Required_Modules:
+			exec(urllib.request.urlopen('https://raw.githubusercontent.com/Bob620/Bob_Bot/master/bobbot/modules/'+ModuleName+'.py').read().decode('utf8'), Running_Modules.__dict__)
+
+	elif module == 'all':
+		All_Modules = Running_Modules.fileInteraction.readfile('info', 'json')['modules']['all']
+		for ModuleName in All_Modules:
+			exec(urllib.request.urlopen('https://raw.githubusercontent.com/Bob620/Bob_Bot/master/bobbot/modules/'+ModuleName+'.py').read().decode('utf8'), Running_Modules.__dict__)
+
+	else:
+		Modules = Running_Modules.fileInteraction.readfile('info', 'json')['modules']
+		if Modules['all'].count(module) >= 1:
+			exec(urllib.request.urlopen('https://raw.githubusercontent.com/Bob620/Bob_Bot/master/bobbot/modules/'+module+'.py').read().decode('utf8'), Running_Modules.__dict__)
+			modules['all'].append(module)
+			if Modules['required'].count(module) >= 1:
+				modules['required'].append(module)
+
+	pprint.PrettyPrinter(indent=2).pprint(modules)
+
+Importation('all')
--- bobbot/modules/fileInteraction.py
+++ bobbot/modules/fileInteraction.py
@@ -1,50 +1,53 @@
 ##### FILE INTERACTION #####
 # So, what I did in 300 lines can be done with json and 30 lines, Im ok with that!
+# Only edits and reads the Info found locally for specifics such as 'personal' info and just about anything else
 # Can read any data file that is given to it
 
 print('Loading: fileInteraction')
 
-def readfile(FileName, Type):
-	try:
-		Type = Type.lower()
-	except:
-		Type = 'json'
-	try:
-		FileName = FileName.lower()
-		
-		if Type == 'json':
-			os.chdir(r'C:/bob_bot/json/data')
-			with codecs.open(FileName+'.json', 'a', encoding='utf8') as File_Make:
-				pass
-			with codecs.open(FileName+'.json', 'r', encoding='utf8') as File_Open:
-				File = File_Open.readline()
-				if File != '':
-					return json.loads(File)
-				else:
-					return {}
-	except Exception as inf:
-		print(inf)
-		return ''
+class fileInteraction():
+	def readfile(FileName, Type):
+		try:
+			Type = Type.lower()
+		except:
+			Type = 'json'
+		try:
+			FileName = FileName.lower()
+			
+			if Type == 'json':
+				os.chdir(r'C:/bob_bot/json/data')
+				with codecs.open(FileName+'.json', 'a', encoding='utf8') as File_Make:
+					pass
+				with codecs.open(FileName+'.json', 'r', encoding='utf8') as File_Open:
+					File = File_Open.readline()
+					if File != '':
+						return json.loads(File)
+					else:
+						return {}
+		except Exception as inf:
+			print(inf)
+			return ''
 
-# Can write, create, and overwrite, any data file that is given to it
-def writefile(FileName, Type, Info):
-	try:
-		Type = Type.lower()
-	except:
-		Type = 'json'
-	try:
-		FileName = FileName.lower()
-		
-		if Type == 'json':
-			os.chdir(r'C:/bob_bot/json/data')
-			with codecs.open(FileName+'.json', 'a', encoding='utf8') as File_Make:
-				pass
-			with codecs.open(FileName+'.json', 'w', encoding='utf8') as File_Open:
-					File_Open.write(json.dumps(Info))
-		return
-	except Exception as inf:
-		print(inf)
-		return
-##### ///// #####
+	# Can write, create, and overwrite, any data file that is given to it
+	def writefile(FileName, Type, Info):
+		try:
+			Type = Type.lower()
+		except:
+			Type = 'json'
+		try:
+			FileName = FileName.lower()
+			
+			if Type == 'json':
+				os.chdir(r'C:/bob_bot/json/data')
+				with codecs.open(FileName+'.json', 'a', encoding='utf8') as File_Make:
+					pass
+				with codecs.open(FileName+'.json', 'w', encoding='utf8') as File_Open:
+						File_Open.write(json.dumps(Info))
+			return
+		except Exception as inf:
+			print(inf)
+			return
 
 print('--LOADED--')
+
+##### ///// #####
