cbe2e35db3e56af38ad07984d70f7eabca0a0568
Bob620 <bruder.kraft225@gmail.com>
2015-04-23 16:26:28 -0500

    Recreating Bobbot to take external files as modules

---
+++ bobbot/main/BobBot.py
@@ -0,0 +1,28 @@
+from socketIO_client import SocketIO, BaseNamespace
+import threading
+import time
+import math
+import random
+import os
+import codecs
+import subprocess
+import time
+import random
+import json
+import importlib
+import sys
+import re
+import collections
+import irc
+import urllib.request
+import imp
+
+fileInteraction = imp.new_module('fileInteraction')
+exec(urllib.request.urlopen('https://raw.githubusercontent.com/Bob620/Bob_Bot/master/bobbot/modules/fileInteraction.py').read().decode('utf8'), Test1.__dict__)
+
+fileInteraction.
+
+__module_name__ = "WaifuBot for Toka"
+__module_version__ = "1.0"
+__module_description__ = "Version 1.0"
+__module_author__ = "Bob620"
