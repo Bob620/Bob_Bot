e22ed2533cd2dae0f6a98e241ff45f8e39626ed4
Bob620 <bruder.kraft225@gmail.com>
2015-04-24 18:23:15 -0500

    Final Main update

--- bobbot/main/BobBot.py
+++ bobbot/main/BobBot.py
@@ -23,6 +23,13 @@ __module_version__ = "20"
 __module_description__ = "The 20th version of BobBot made by Bob620, I may have skipped a few numbers but it really is close to that >.>... At least every version gets better, usually."
 __module_author__ = "Bob620"
 
+##### Modules #####
+# This bit will make sure everything can call the module lists
+
+# When called, Importation will load the selected module, or lists of modules, and load each one, If the module is already loaded
+# it will then reaload the module in the same fasion it would load ne normally. Once loaded the module's name will be logged in
+# the loaded/running module list.
+
 global modules
 modules = {"required":[], "all":[], "extra":[]}
 global Running_Modules
@@ -35,6 +42,14 @@ def Importation(module):
 		modules['required'].append('fileInteraction')
 		modules['all'].append('fileInteraction')
 
+	if module == 'fileInteraction':
+		print('')
+		print('------LOAD------')
+		pprint.PrettyPrinter(indent=2).pprint(modules)
+		print('----------------')
+		print('')
+		return
+
 	if module == 'required':
 		Required_Modules = Running_Modules.fileInteraction.readfile('info', 'json')['modules']['required']
 		for ModuleName in Required_Modules:
@@ -43,21 +58,61 @@ def Importation(module):
 			modules['required'].append(module)
 
 	elif module == 'all':
-		All_Modules = Running_Modules.fileInteraction.readfile('info', 'json')['modules']['all']
-		for ModuleName in All_Modules:
+		Modules_List = Running_Modules.fileInteraction.readfile('info', 'json')['modules']
+		for ModuleName in Modules_List['all']:
 			exec(urllib.request.urlopen('https://raw.githubusercontent.com/Bob620/Bob_Bot/master/bobbot/modules/'+ModuleName+'.py').read().decode('utf8'), Running_Modules.__dict__)
 			modules['all'].append(module)
-			if Modules['required'].count(module) >= 1:
+			if Modules_List['required'].count(module) < 1:
 				modules['required'].append(module)
 
 	else:
-		Modules = Running_Modules.fileInteraction.readfile('info', 'json')['modules']
-		if Modules['all'].count(module) >= 1:
+		Modules_List = Running_Modules.fileInteraction.readfile('info', 'json')['modules']
+		if Modules_List['all'].count(module) < 1:
 			exec(urllib.request.urlopen('https://raw.githubusercontent.com/Bob620/Bob_Bot/master/bobbot/modules/'+module+'.py').read().decode('utf8'), Running_Modules.__dict__)
 			modules['all'].append(module)
-			if Modules['required'].count(module) >= 1:
+			if Modules_List['required'].count(module) < 1:
 				modules['required'].append(module)
 
+	print('')
+	print('------LOAD------')
 	pprint.PrettyPrinter(indent=2).pprint(modules)
+	print('----------------')
+	print('')
 
+def Deimportation(module):
+
+	if modules['all'].count('fileInteraction') < 1:
+		exec(urllib.request.urlopen('https://raw.githubusercontent.com/Bob620/Bob_Bot/master/bobbot/modules/fileInteraction.py').read().decode('utf8'), Running_Modules.__dict__)
+		modules['required'].append('fileInteraction')
+		modules['all'].append('fileInteraction')
+	Modules_List = Running_Modules.fileInteraction.readfile('info', 'json')['modules']
+
+	if module == 'all':
+		for ModuleName in Modules_List['all']:
+			del Running_Modules.__dict__[ModuleName]
+			modules['all'].remove(ModuleName)
+			if Modules_List['required'].count(module) < 1:
+				modules['required'].remove(ModuleName)
+
+
+	else:
+		if modules['all'].count(module) >= 1:
+			del Running_Modules.__dict__[module]
+			modules['all'].remove(module)
+			if Modules_List['required'].count(module) < 1:
+				modules['required'].remove(module)
+
+	print('')
+	print('-----UNLOAD-----')
+	pprint.PrettyPrinter(indent=2).pprint(modules)
+	print('----------------')
+	print('')
+
+## Import everything ##
 Importation('all')
+## UnImport fileInteraction for a load test ##
+Deimportation('fileInteraction')
+## Re-import just incase ##
+Importation('fileInteraction')
+
+##### ///// #####
---
+++ bobbot/modules/mod.py
@@ -0,0 +1,47 @@
+##### Moding #####
+# Allows a mod to add more mods(Temp)
+
+class mod():
+
+	def mod(Channel, Text):
+		try:
+			if len(Text) < 1:
+				return '!mod NAME [NAME NAME ...]'
+			PossibleCommands = Running_Modules.fileInteraction.readfile('Commands', 'json')
+			NameList = ''
+			for Name in Text:
+				PossibleCommands['channels'][Channel]['ops'].append(Name.lower())
+				NameList = NameList + Name.lower() + ', '
+			Running_Modules.fileInteraction.writefile('commands', 'json', PossibleCommands)
+			if len(Text) == 1:
+				return NameList.strip(', ')+' has been modded!'
+			return NameList.strip(', ')+' have been modded!'
+		except Exception as inf:
+			print(inf)
+			return 'An error returned during modding! -Person/People most likely not modded-'
+	
+	# Allows a mod to remove other mods(Temp)
+	def unmod(Channel, Text):
+		try:
+			if len(Text) < 1:
+				return '!unmod NAME'
+			PossibleCommands = Running_Modules.fileInteraction.readfile('Commands', 'json')
+			for i in range(0, len(PossibleCommands['channels'][Channel]['ops'])-1):
+				if Text[0] == PossibleCommands['channels'][Channel]['ops'][i]:
+					PossibleCommands['channels'][Channel]['ops'].pop(i)
+			Running_Modules.fileInteraction.writefile('commands', 'json', PossibleCommands)
+			return Text[0]+' has been unmodded'
+		except:
+			return 'An error has returned during unmodding! -Person most likely not unmodded-'
+	
+	# Shows the list of mods
+	def modlist(Channel, Text):
+		try:
+			PossibleCommands = Running_Modules.fileInteraction.readfile('Commands', 'json')
+			if len(PossibleCommands['channels'][Channel]['ops']) == 0:
+				return 'There are no mods on this channel'
+			if len(PossibleCommands['channels'][Channel]['ops']) == 1:
+				return ', '.join(PossibleCommands['channels'][Channel]['ops']) + ' is the mod on this channel'
+			return ', '.join(PossibleCommands['channels'][Channel]['ops']) + ' are mods on this channel'
+		except:
+			return "Unable to return channel's mod list!"
